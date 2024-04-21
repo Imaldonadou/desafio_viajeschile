886016b8d0cce5f2e653f7efc792aaa8ef5f426d
aknasho <igmaldonadou@gmail.com>
2024-04-20 23:50:22 -0400

    se actualiza cambios que existiecen

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,21 @@ window.addEventListener("scroll", () => {
       : nav.classList.remove("bg-dark")
   })
   
+  // form submit
+document.getElementById("formulario").addEventListener("submit", (e) => {
+  e.preventDefault();
+  const date = new Date(Date.now());
+  const popup = document.getElementById('popup');
+  document.getElementById("date").textContent = date.toLocaleString();
+  const toast = bootstrap.Toast.getOrCreateInstance(popup);
+  toast.show()
+  setTimeout(() => {
+      toast.hide()
+  }, 2500)
+  e.target.reset();
+})
+$(document).ready(function () {
+  // habilitar todos los tooltip de bootstrap
+  $('[data-bs-toggle="tooltip"]').tooltip()
+})
+
